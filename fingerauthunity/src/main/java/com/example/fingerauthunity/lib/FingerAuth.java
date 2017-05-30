dcda9245b7456e331d5732c10fdb1ad74847fe52
MizoTake <mizotake123@gmail.com>
2017-05-30 23:52:34 +0900

    first

---
+++ fingerauthunity/src/main/java/com/example/fingerauthunity/lib/FingerAuth.java
@@ -0,0 +1,53 @@
+package com.example.fingerauthunity.lib;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.os.CancellationSignal;
+import android.os.Handler;
+import android.util.Log;
+
+import com.unity3d.player.UnityPlayer;
+
+import static android.content.Context.FINGERPRINT_SERVICE;
+
+
+/**
+ * Created by TakeruDesk on 2017/05/30.
+ */
+
+public class FingerAuth {
+
+    private static CancellationSignal cancel;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static void AuthStart(Context context) {
+        UnityPlayer.UnitySendMessage("FingerManager", "FingerAuthResult", "0");
+        FingerprintManager fingerprintManager = (FingerprintManager) context.getSystemService(FINGERPRINT_SERVICE);
+        cancel = new CancellationSignal();
+        fingerprintManager.authenticate(null, cancel, 0, new FingerprintManager.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+                UnityPlayer.UnitySendMessage("FingerManager", "FingerAuthResult", "1");
+                cancel.cancel();
+                Log.i("", "auth success");
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                UnityPlayer.UnitySendMessage("FingerManager", "FingerAuthResult", "2");
+                cancel.cancel();
+                Log.e("", "failed");
+            }
+            /*
+            @Override
+            public void onAuthenticationError(int errorCode, CharSequence errString) {
+                UnityPlayer.UnitySendMessage("FingerManager", "FingerAuthResult", "3");
+                cancel.cancel();
+                Log.e("", "error " + errorCode + " " + errString);
+            }
+            */
+        }, new Handler());
+    }
+}
